b7cacfdf5e5f18393f5661a90dbdfbee2d8f5bd5
joe max <joe.max@tutanota.com>
2023-06-16 12:51:37 -0400

    ready to start final after some fixes

--- src/App.js
+++ src/App.js
@@ -19,30 +19,32 @@ const App = () => {
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
                         {/* Add Budget component here under */}
-                    <div className='col-sm'>
-                        <Budget />
-                    </div>
+                        <div className='col-sm'>
+                            <Budget />
+                        </div>
                         {/* Add Remaining component here under */}        
-                    <div className='col-sm'>
-                        <Remaining />
-                    </div>
+                        <div className='col-sm'>
+                            <Remaining />
+                        </div>
                         {/* Add ExpenseTotal component here under */}        
-                    <div className='col-sm'>
-                        <ExpenseTotal />
-                    </div>                       
+                        <div className='col-sm'>
+                            <ExpenseTotal />
+                        </div>
+                    </div>
+                
+                    <div>
+                    <h2 className='mt-3'>Allocation</h2>
                         {/* Add ExpenseList component here under */}         
-                    <div className='col-sm'>
-                        <ExpenseList />
-                    </div>   
-                        {/* Add ExpenseItem component here under */}        
-                    <div className='col-sm'>
-                        <ExpenseItem />
-                    </div>   
+                        <div className='col-sm'>
+                            <ExpenseList />
+                        </div>   
+
+                    <h2 className='mt-3'>Change Allocation</h2>   
                         {/* Add AllocationForm component here under */}        
-                    <div className='col-sm'>
-                        <AllocationForm />
-                    </div>   
-                </div>
+                        <div className='col-sm'>
+                            <AllocationForm />
+                        </div>   
+                    </div>
             </div>
         </AppProvider>
     );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
-import { TiDelete } from 'react-icons/ti';
+import { TiDelete, TiMinusOutline, TiPlusOutline } from 'react-icons/ti';
+
 import { AppContext } from '../context/AppContext';
 const ExpenseItem = (props) => {
     const { dispatch } = useContext(AppContext);
@@ -19,11 +20,22 @@ const ExpenseItem = (props) => {
             payload: expense
         });
     }
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+    }
     return (
         <tr>
         <td>{props.name}</td>
         <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td><TiPlusOutline size='1.5em' onClick={event=> increaseAllocation(props.name)}></TiPlusOutline></td>
+        <td><TiMinusOutline size='1.5em' onClick={event=> decreaseAllocation(props.name)}></TiMinusOutline></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
